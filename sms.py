646a549ba2ac5ee4354e33c434f94ba0c2a2819c
Joel Holdsworth <joel.holdsworth@vcatechnology.com>
2014-10-17 13:43:14 +0100

    sms.py: Made SMS more reliable

--- sms.py
+++ sms.py
@@ -8,35 +8,58 @@ from pyxmpp2.message import Message
 from pyxmpp2.client import Client
 from pyxmpp2.settings import XMPPSettings
 from pyxmpp2.interfaces import EventHandler, event_handler, QUIT
+from pyxmpp2.interfaces import XMPPFeatureHandler
 from pyxmpp2.streamevents import AuthorizedEvent, DisconnectedEvent
+from pyxmpp2.interfaces import presence_stanza_handler, message_stanza_handler
 
-class SmsRobotHandler(EventHandler):
-    def __init__(self, target_jid, messages):
+class SmsRobotClient(EventHandler, XMPPFeatureHandler):
+    def __init__(self, your_jid, your_pass, target_jid, messages):
         self.target_jid = target_jid
         self.messages = messages
 
+        self.connected = False
+        self.established = False
+
+        settings = XMPPSettings({
+            u"password": your_pass,
+            u"starttls": True,
+            u"tls_verify_peer": False,
+        })
+        self.client = Client(JID(your_jid), [self], settings)
+        self.client.connect()
+
+    def run(self):
+        while self.messages:
+            if self.connected and not self.established:
+                print("Connecting...")
+                self.client.stream.send(Message(to_jid = self.target_jid,
+                    body = "?", stanza_type = 'chat'))
+            self.client.run(timeout = 2)
+
     @event_handler(AuthorizedEvent)
     def handle_authorized(self, event):
-	for m in self.messages:
-            event.stream.send(Message(to_jid = self.target_jid,
-                body = 'sms:%s:%s' % m, stanza_type = 'chat'))
-        event.stream.disconnect()
+        self.connected = True
+
+    @message_stanza_handler()
+    def handle_message(self, stanza):
+        if not self.established:
+            if '"help"' not in stanza.body:
+                return
+            self.established = True
+
+        if not self.messages:
+            self.client.disconnect()
+            return None
+
+        body = 'sms:%s:%s' % self.messages.pop()
+        print(body)
+        return Message(to_jid = self.target_jid,
+            body = body, stanza_type = 'chat')
 
     @event_handler(DisconnectedEvent)
     def handle_disconnected(self, event):
         return QUIT
-    
-    @event_handler()
-    def handle_all(self, event):
-        logging.info(u"-- {0}".format(event))
 
 def send_messages(your_jid, your_pass, target_jid, messages):
-    handler = SmsRobotHandler(JID(target_jid), messages)
-    settings = XMPPSettings({
-        u"password": your_pass,
-        u"starttls": True,
-        u"tls_verify_peer": False,
-    })
-    client = Client(JID(your_jid), [handler], settings)
-    client.connect()
-    client.run()
+    c = SmsRobotClient(your_jid, your_pass, JID(target_jid), messages)
+    c.run()
